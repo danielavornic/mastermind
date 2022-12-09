ca763e97dd7ac456662f93dffb172d07b2511c26
Daniela Vornic <daniela.vornic@gmail.com>
2022-12-09 11:20:53 +0200

    Init game context

---
+++ src/context/Game.tsx
@@ -0,0 +1,55 @@
+import { createContext, useState } from "react";
+import { Clue, Code, GameMode, Guess } from "types";
+
+interface GameContext {
+  mode: GameMode;
+  setMode: (mode: GameMode) => void;
+  code?: Code;
+  setCode: (code: Code) => void;
+  guess?: Code;
+  setGuess: (guess: Code) => void;
+  clue?: Clue;
+  setClue: (clue: Clue) => void;
+  history?: Guess[];
+  setHistory: (history: Guess[]) => void;
+}
+
+export const GameContext = createContext<GameContext>({
+  mode: GameMode.AUTO,
+  setMode: () => {},
+  code: undefined,
+  setCode: () => {},
+  guess: undefined,
+  setGuess: () => {},
+  clue: undefined,
+  setClue: () => {},
+  history: [],
+  setHistory: () => {},
+});
+
+export const GameProvider = ({ children }: { children: React.ReactNode }) => {
+  const [mode, setMode] = useState<GameMode>(GameMode.AUTO);
+  const [code, setCode] = useState<Code>();
+  const [guess, setGuess] = useState<Code>();
+  const [clue, setClue] = useState<Clue>();
+  const [history, setHistory] = useState<Guess[]>([]);
+
+  return (
+    <GameContext.Provider
+      value={{
+        mode,
+        setMode,
+        code,
+        setCode,
+        guess,
+        setGuess,
+        history,
+        setHistory,
+        clue,
+        setClue,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
---
+++ src/types/game.ts
@@ -0,0 +1,32 @@
+export enum GameMode {
+  AUTO,
+  MANUAL,
+}
+
+export enum Colors {
+  RED = "red",
+  GREEN = "green",
+  BLUE = "blue",
+  YELLOW = "yellow",
+  ORANGE = "orange",
+  PURPLE = "purple",
+}
+
+export enum ClueColor {
+  WHITE = "white",
+  BLACK = "black",
+}
+
+export type Color = keyof typeof Colors;
+
+export type Code = Color[];
+
+export type Clue = {
+  color: ClueColor;
+  count: number;
+};
+
+export type Guess = {
+  code: Code;
+  clue: Clue;
+};
